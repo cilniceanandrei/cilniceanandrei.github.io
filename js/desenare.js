8c196d82bf24017216c5cd1ee88c723ef56b26ad
cilniceanandrei <44093487+cilniceanandrei@users.noreply.github.com>
2019-01-24 21:11:51 +0200

    Update desenare.js

--- js/desenare.js
+++ js/desenare.js
@@ -1,22 +1,46 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version=2019.01.11.0";
-document.getElementById("id_button").addEventListener("click", on_get_cam);
+document.getElementById("id_business_version").innerHTML = "Business version 2018.10.26.6"
+document.getElementById("id_start").addEventListener("click", start);
+document.getElementById("id_stop").addEventListener("click", stop);
 
-function on_cam_error(e){
-    alert("cam error");
-}
-function on_cam_ok(e){
-    document.getElementById("id_video").srcObject = e;
-}
-function on_get_cam(){
-    var c = {audio: true, video:true};
-   // navigator.mediaDevices.getUserMedia(c, on_cam_ok, on_cam_error);
+document.getElementById("id_start").disabled = false;
+document.getElementById("id_stop").disabled = true;
 
-    navigator.mediaDevices.getUserMedia(c)
-    .then(function(stream) {
-    /* use the stream */
-    document.getElementById("id_video").srcObject = stream;
-    })
-    .catch(function(err) {
-    /* handle the error */
-    });
+document.getElementById("id_stop").addEventListener("click", stop);
+
+var alpha = {unghi:0};
+function deseneazaCerc(context, w, h,alpha){
+    
+    alpha.unghi++;
+    context.clearRect(0, 0, w, h);
+
+    var r = 100;
+    
+    var x = w / 2 + r * Math.cos(alpha.unghi * Math.PI  / 180);
+    var y = h / 2 + r * Math.sin(alpha.unghi * Math.PI  / 180);
+        
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.stroke();
+    
+}
+
+function start(){
+    document.getElementById("id_start").disabled = true;
+    document.getElementById("id_stop").disabled = false;
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+    
+    intervalId = setInterval(  deseneazaCerc, 10, context, canvas.width, canvas.height, alpha, 100);
+
+    var my_worker = new Worker("js/calcul_prime.js");
+    my_worker.onmessage = function(e){
+        document.getElementById("id_prime").innerHTML = e.data;
+    }
+    
+}
+
+function stop(){
+    clearInterval(intervalId);
+    document.getElementById("id_start").disabled = false;
+    document.getElementById("id_stop").disabled = true;
 }
